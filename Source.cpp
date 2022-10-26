1082f5534239c05df0982b066a1e92434b0b4eed
Aleksey <113472646+Torcna@users.noreply.github.com>
2022-10-26 14:39:40 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,82 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n,u, c,p,k,o,count=0,num=0;
+	cin >> n;
+
+	char* str = new char[n];
+
+	for (int i = 0; i < n; i++)
+	{
+		cin>>str[i];
+	}
+
+	cin >> p;
+	char* p_str = new char[p];
+
+	for (int i = 0; i < p; i++)
+	{
+		cin >> p_str[i];
+	}
+
+	cin >> u;
+	char* nstr = new char[u];
+	for (int i = 0; i < u; i++)
+	{
+		cin >> nstr[i];
+	}
+	
+	for (int i = 0; i <= n - p; i++)
+	{
+		k = 0;
+		for (int j = 0; j < p; j++)
+		{
+			if (str[i + j] == p_str[j])
+			{
+				k++;
+			}
+		}
+		if (k == p)
+		{
+			count++;
+			i += p-1;
+		}
+	}
+	
+	o = n - count * p + count * u;
+
+	char* ans = new char[o];
+	for (int i = 0; i < n; i++)
+	{
+		k = 0;
+		for (int j = 0; j < p; j++)
+		{
+			if (str[i + j] == p_str[j])
+			{
+				k++;
+			}
+		}
+		if (k == p)
+		{
+			count++;
+			for (int z = 0; z < u; z++)
+			{
+				ans[num] = nstr[z];
+				num++;
+			}
+			i += p;
+		}
+		else
+		{
+			ans[num] = str[i];
+			num++;
+		}
+	}
+	for (int i = 0; i < o; i++)
+	{
+		cout << ans[i];
+	}
+}
